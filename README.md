773d37d7aa65891ebdad6595affdaef1208b8c3e
Skyliner HQ <skylinerhq@skyliner.io>
2017-02-22 23:00:01 +0000

    initial commit of Skyliner app template

---
+++ README.md
@@ -0,0 +1,9 @@
+# slack-command-flask
+
+This is a demo Python ([Flask](http://flask.pocoo.org/)) app you can deploy to [Skyliner](https://www.skyliner.io) to implement a simple Slack [slash command](https://api.slack.com/slash-commands). Here's a guide to getting started:
+
+[https://www.skyliner.io/help/quick-start](https://www.skyliner.io/help/quick-start)
+
+If you have any trouble, please drop us a line at [support@skyliner.io](mailto:support@skyliner.io?Subject=Help%20with%20hello-flask), or feel free to join the [Skyliner support Slack](https://slack.skyliner.io).
+
+Repo created by [Skyliner](https://www.skyliner.io) app templates at 2017-02-22T23:00:01.267Z
---
+++ commands.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request, jsonify
+import os
+import requests
+
+app = Flask('slack-command-flask')
+
+def random_wikipedia_article():
+    resp = requests.post(
+        'https://en.wikipedia.org/wiki/Special:RandomInCategory',
+        data={ 'wpcategory': 'Dog breeds' },
+        allow_redirects=False)
+    return resp.headers['Location']
+
+@app.route('/command', methods=['POST'])
+def command():
+    if request.form.get('token') != os.getenv('SLACK_COMMAND_TOKEN'):
+        # TODO:
+        # You should verify that the request is coming from your Slack
+        # instance if you return anything more interesting than a Wikipedia
+        # article about dogs.
+        #
+        # To find your token, visit the configuration page. Go to:
+        # https://<your slack>.slack.com/apps/manage/custom-integrations
+        #
+        # Then choose Slash Commands > Edit configuration (for this command).
+        # Save this token in your Skyliner environment variables for this app
+        # as SLACK_COMMAND_TOKEN.
+        #
+        # You can then enable verification by uncommenting the following
+        # line and removing the pass statement below it.
+        #
+        # return 'Unauthorized', 401
+        pass
+
+    return jsonify({
+        'response_type': 'in_channel',
+        'text': ('It works! Here is a random wiki about dogs: %s' %
+                 random_wikipedia_article()),
+    })
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('localhost')
